5571070d74f4140c34ff6714a9fa20a0a92381ab
Tommy Luong <tl-mb@Tommys-MacBook-Pro.local>
2021-05-18 21:54:58 -0400

    resubmission of PWGEN

--- README.md
+++ README.md
@@ -90,3 +90,4 @@ You are required to submit the following for review:
 
 - - -
 © 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+![passwordgeneratorappimage](image.png)
---
+++ script.js
@@ -0,0 +1,86 @@
+// Elements
+const pwEl = document.getElementById("pw");
+const copyEl = document.getElementById("copy");
+const lenEl = document.getElementById("len");
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+const generateEl = document.getElementById("genBtn");
+// uppercase, lowercase, numbers, and symbols
+const upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = ["@", "!", "#", "%", "$", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "[", "]", "}", "|", ":", ";", "'", "<", ",", ".", ">", "/", "?"];
+
+// grabbing the specified characters
+function getLowercase() {
+    return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+}
+
+function getUppercase() {
+    return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+
+function getNumber() {
+    return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getSymbol() {
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function generatePassword() {
+    const len = lenEl.value;
+
+    let password = "";
+
+    if (upperEl.checked) {
+        password += getUppercase();
+    }
+
+    if (lowerEl.checked) {
+        password += getLowercase();
+    }
+
+    if (numberEl.checked) {
+        password += getNumber();
+    }
+
+    if (symbolEl.checked) {
+        password += getSymbol();
+    }
+
+    for (let i = password.length; i < len; i++) {
+        const x = generateX();
+        password += x;
+    }
+
+    pwEl.innerText = password;
+}
+
+function generateX() {
+    const xs = [];
+    if (upperEl.checked) {
+        xs.push(getUppercase());
+    }
+
+    if (lowerEl.checked) {
+        xs.push(getLowercase());
+    }
+
+    if (numberEl.checked) {
+        xs.push(getNumber());
+    }
+
+    if (symbolEl.checked) {
+        xs.push(getSymbol());
+    }
+
+    if (xs.length === 0) return "";
+
+    return xs[Math.floor(Math.random() * xs.length)];
+}
+// event to generate a password when clicked
+generateEl.addEventListener("click", generatePassword);
+
